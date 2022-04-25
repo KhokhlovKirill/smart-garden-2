9c83451c42ac1438ac43c4033adfb0c4069801df
KhokhlovKirill <38361403+KhokhlovKirill@users.noreply.github.com>
2022-04-25 11:02:29 +0300

    Update smart-garden-2nd.ino

--- Device/smart-garden-2nd.ino
+++ Device/smart-garden-2nd.ino
@@ -1,18 +1,17 @@
 #include <Wire.h>
 void setup() {
- Wire.begin(8);                /* join i2c bus with address 8 */
- Wire.onReceive(receiveEvent); /* register receive event */
- Wire.onRequest(requestEvent); /* register request event */
- Serial.begin(9600);           /* start serial comm. */
- Serial.println("I am I2C Slave");
+ Wire.begin(8);
+ Wire.onReceive(receiveEvent);
+ Wire.onRequest(requestEvent);
+ Serial.begin(9600);
 }
 void loop() {
  delay(100);
 }
-// function that executes whenever data is received from master
+
 void receiveEvent(int howMany) {
- while (0 <Wire.available()) {
-    byte c = Wire.read();      /* receive byte as a character */
+ while (0 < Wire.available()) {
+    byte c = Wire.read();
     Serial.println(c);
     switch (c){
       case 0:
@@ -27,7 +26,7 @@ void receiveEvent(int howMany) {
       }
   }    
 }
-// function that executes whenever data is requested from master
+
 void turnOnRedLED(){
   
   }
